3ca84de47c63cc7d32956befc69bfc13fd033826
joshtorres <joshtorres@users.noreply.github.com>
2023-10-04 16:39:32 +0000

    Fix styling

--- src/CatalystLocationServiceProvider.php
+++ src/CatalystLocationServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use OmniaDigital\CatalystLocation\Commands\CatalystLocationCommand;
+use OmniaDigital\CatalystLocation\Testing\TestsCatalystLocation;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use OmniaDigital\CatalystLocation\Commands\CatalystLocationCommand;
-use OmniaDigital\CatalystLocation\Testing\TestsCatalystLocation;
 
 class CatalystLocationServiceProvider extends PackageServiceProvider
 {
